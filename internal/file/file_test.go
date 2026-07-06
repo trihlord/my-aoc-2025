40afc2f38d86ff01020b48645d08a1630962c28c
Vladimir Kubantsev <trihlord@gmail.com>
2026-07-06 09:00:23 +0300

    refactor(internal, file): test data structure

--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -15,17 +15,17 @@ func TestReadLines(t *testing.T) {
 	}{
 		{
 			desc: "returns empty line",
-			name: filepath.Join("testdata", "empty.txt"),
+			name: filepath.Join("testdata", "split", "lines", "empty.txt"),
 			line: "",
 		},
 		{
-			desc: "returns single line",
-			name: filepath.Join("testdata", "single_line.txt"),
+			desc: "returns one line",
+			name: filepath.Join("testdata", "split", "lines", "one.txt"),
 			line: "single",
 		},
 		{
 			desc: "returns joined two lines",
-			name: filepath.Join("testdata", "two_lines.txt"),
+			name: filepath.Join("testdata", "split", "lines", "two.txt"),
 			line: "foobar",
 		},
 	}
@@ -33,6 +33,51 @@ func TestReadLines(t *testing.T) {
 		t.Run(tC.desc, func(t *testing.T) {
 			lines := []string{}
 			for line, err := range ReadLines(tC.name) {
+				if err != nil {
+					t.Fatal(err)
+				}
+				lines = append(lines, line)
+			}
+			if got, want := strings.Join(lines, ""), tC.line; got != want {
+				t.Fatalf("got %q, want %q", got, want)
+			}
+		})
+	}
+}
+
+func TestReadCommas(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		desc string
+		name string
+		line string
+	}{
+		{
+			desc: "returns empty line",
+			name: filepath.Join("testdata", "split", "commas", "empty.txt"),
+			line: "",
+		},
+		{
+			desc: "returns one line",
+			name: filepath.Join("testdata", "split", "commas", "none.txt"),
+			line: "foo",
+		},
+		{
+			desc: "returns joined one comma",
+			name: filepath.Join("testdata", "split", "commas", "one.txt"),
+			line: "foobar",
+		},
+		{
+			desc: "returns joined two commas",
+			name: filepath.Join("testdata", "split", "commas", "two.txt"),
+			line: "foobarbaz",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			t.Parallel()
+			lines := []string{}
+			for line, err := range ReadCommas(tC.name) {
 				if err != nil {
 					t.Fatal(err)
 				}
