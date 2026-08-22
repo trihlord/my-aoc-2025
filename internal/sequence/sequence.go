2599c46fc63b35834538da210ad00bfc75e21f91
Vladimir Kubantsev <trihlord@gmail.com>
2026-08-22 20:34:33 +0300

    feat(internal, sequence): refactor monotonic stack

--- internal/sequence/sequence.go
+++ internal/sequence/sequence.go
@@ -1,21 +1,19 @@
 package sequence
 
-import "bytes"
-
 func Max(digits string, capacity int) string {
-	stk := []byte(digits)[:capacity:capacity]
-Loop:
-	for i := len(stk); i < len(digits); i++ {
-		for j := 0; j < len(stk); j++ {
-			otherStk := make([]byte, 0, len(stk))
-			otherStk = append(otherStk, stk[:j]...)
-			otherStk = append(otherStk, stk[j+1:]...)
-			otherStk = append(otherStk, digits[i])
-			if bytes.Compare(otherStk, stk) == 1 {
-				stk = otherStk
-				continue Loop
-			}
+	stk := make([]byte, 0, capacity)
+	dpth := len(digits) - capacity
+	for i := 0; i < len(digits); i++ {
+		char := digits[i]
+		for len(stk) > 0 && char > stk[len(stk)-1] && dpth > 0 {
+			stk = stk[:len(stk)-1]
+			dpth--
 		}
+		if len(stk) < capacity {
+			stk = append(stk, char)
+			continue
+		}
+		dpth--
 	}
 	return string(stk)
 }
