555dad9cabfb80ef37905ce701e55be3591a0756
Vladimir Kubantsev <trihlord@gmail.com>
2026-08-22 20:01:18 +0300

    feat(internal, sequence): return expected

--- internal/sequence/sequence.go
+++ internal/sequence/sequence.go
@@ -1,5 +1,21 @@
 package sequence
 
-func Max(str string, cap int) string {
-	return ""
+import "bytes"
+
+func Max(digits string, capacity int) string {
+	stk := []byte(digits)[:capacity:capacity]
+Loop:
+	for i := len(stk); i < len(digits); i++ {
+		for j := 0; j < len(stk); j++ {
+			otherStk := make([]byte, 0, len(stk))
+			otherStk = append(otherStk, stk[:j]...)
+			otherStk = append(otherStk, stk[j+1:]...)
+			otherStk = append(otherStk, digits[i])
+			if bytes.Compare(otherStk, stk) == 1 {
+				stk = otherStk
+				continue Loop
+			}
+		}
+	}
+	return string(stk)
 }
